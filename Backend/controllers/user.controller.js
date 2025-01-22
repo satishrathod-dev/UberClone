c0f1de4f6cb846e22ea947d9f72d4081dfa4f166
Satish Rathod <satishrathod.dev@gmail.com>
2025-01-22 14:34:23 +0530

    profile middleware added

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -47,6 +47,11 @@ module.exports.loginUser = async (req, res, next) => {
   }
 
   const token = user.generateJwtToken();
+  res.cookie("token", token);
 
   res.status(200).json({ token, user });
+};
+
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
 };
